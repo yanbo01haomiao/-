a11decb8ef21cd5ca6ecafc7e4cb7b12e6da541e
zhangyuanes <zhangyuanes@gmail.com>
2019-09-23 11:15:23 +0800

    init js

---
+++ server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'COntent-Type':'text/plain'});
+    res.end('hello node.js');
+}).listen(3000, 'localhost', function () {
+    console.log('Server running at http://localhost:3000');
+});
